31cf22ba16dbde5358d92cf6681c9b028c2c4a88
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2014-10-18 21:45:26 -0300

    Aula 4: Trabalhando com filtros

--- js/app.js
+++ js/app.js
@@ -10,14 +10,54 @@
 			price: 2.95,
 			description: ". . .",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			createDate: new Date(),
+			images: [
+				{
+					full: "images/Dodecahdron.png",
+					thumb: "images/Dodecahdron-thumb.png"
+				},
+				{
+					full: "images/Dodecahdron-2.png",
+					thumb: "images/Dodecahdron-thumb-2.png"
+				}
+			]
 		},
 		{
 			name: "Pentagonal gem",
 			price: 5.95,
 			description: ". . .",
 			canPurchase: true,
-			soldOut: false
+			soldOut: false,
+			createDate: new Date(),
+			images: [
+				{
+					full: "images/Pentagonal.png",
+					thumb: "images/Pentagonal-thumb.png"
+				},
+				{
+					full: "images/Pentagonal-2.png",
+					thumb: "images/Pentagonal-thumb-2.png"
+				}
+			]
+		},
+		{
+			name: "Pearl gem",
+			price: 8.95,
+			description: ". . .",
+			canPurchase: true,
+			soldOut: false,
+			createDate: new Date(),
+			images: [
+				{
+					full: "images/Pearl.png",
+					thumb: "images/Pearl-thumb.png"
+				},
+				{
+					full: "images/Pearl-2.png",
+					thumb: "images/Pearl-thumb-2.png"
+				}
+			]
 		}
 	];
 })();
