a40d4ce9fe71becff5d1dce83fa075a01cb4ebe4
Enrique Pereira <enrique.pereira.ssp@gmail.com>
2014-10-19 22:42:03 -0200

    Aula 5 Versao 2: Manipulando CSS e comportamento com ng-class e ng-click

--- js/app.js
+++ js/app.js
@@ -3,6 +3,16 @@
 	app.controller('StoreController', function() {
 		this.products = gems;
 	});
+	app.controller('PanelController', function() {
+		this.tab = 1;
+		this.selectTab = function(selectedTab) {
+			this.tab = selectedTab;
+		};
+		this.isSelected = function(checkTab) {
+			return this.tab === checkTab;
+		}
+
+	});
 
 	var gems = [
 		{
